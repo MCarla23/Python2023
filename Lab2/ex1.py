cb9639050f9e5ebcdf0f31f87ffd8b614869dd71
Adonicioaie Maria-Carla <100224892+MCarla23@users.noreply.github.com>
2023-10-18 16:59:19 +0300

    Add files via upload

--- Lab2/ex1.py
+++ Lab2/ex1.py
@@ -1,21 +1,16 @@
 #ex1
 def fibo(n):
+    if n == 0:
+        print(f'Primul numar din Fibonacci este: [0]')
+        return [0]
     if n == 1:
-        print(f'Primul numar din Fibonacci este: [1]')
-        return [1]
-    if n == 2:
-        print(f'Primele {n} numere din Fibonacci sunt: [1,1]')
-        return [1,1]
-    a = 1
-    b = 1
-    ls = [1,1]
-    i = 2
+        print(f'Primele {n} numere din Fibonacci sunt: [0,1]')
+        return [0,1]
 
+    ls = [0,1]
+    i = 2
     while i < n:
-        aux = a
-        a = b
-        b = aux + b
-        ls.append(b)
+        ls.append(ls[i-1]+ls[i-2])
         i += 1
     print(f'Primele {n} numere din Fibonacci sunt: {ls}')
 
